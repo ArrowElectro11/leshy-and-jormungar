09e645cd5df417cb1b99f3b39bf4a5d50f988340
Jormungar <Gormondgar@gmail.com>
2025-05-28 16:58:34 +1000

    i added a quick test
    added a sprite

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var  _jormungar = 12
+var _jormungar = 12
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
